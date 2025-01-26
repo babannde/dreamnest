14d8815bea1aa292b9c5edf639e921eecedfc8e9
babannde <hamzabande@outlook.com>
2025-01-27 00:10:24 +0100

    First commit

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Stay in the moment <br /> Make 
+        memories
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
